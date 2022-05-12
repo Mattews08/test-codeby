cbda942471d9a4b981f29fde69ec11aeabda9be7
Matheus Nascimento <matheus.n16@bkpi.io>
2022-05-12 08:24:06 -0400

    tirando comentarios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import type {NextPage} from 'next'
 import {Main} from '../components/Main'
 import {CardProduct} from '../components/CardProduct'
-import {useState} from 'react'
 
 const Home: NextPage = () => {
     return (
